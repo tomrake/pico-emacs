02234994da0af7b167df61acbd5b8b2f70c8374b
Tom Rake <zzzap1957@gmail.com>
2025-12-12 20:41:30 -0500

    Changed many functions to have pico- prefix.

--- pico-emacs.el
+++ pico-emacs.el
@@ -1,3 +1,13 @@
+;;; pico-emacs.el --- Tools for Raspberry Pi / Pico coding
+
+;; 
+;; SPDX-FileCopyrightText: Copyright (C) 2025 Thomas W. Rake
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+
+
+
 (defgroup pico-emacs
   nil "Configuration of pico development in emacs."
   :prefix "pico-"
@@ -13,6 +23,16 @@
   :type 'string
   :group 'pico-emacs)
 
+(defcustom openocd-interface "interface/cmsis-dap.cfg"
+  "The interface string for openocd. This is the debugging interface."
+  :type 'string
+  :group 'pico-emacs)
+
+(defcustom openocd-target "target/rp2350.cfg"
+  "The target string for openocd. The is cpu chip.
+   Should be target/rp2350.cfg or target/rp2040"
+  :type 'string
+  :group 'pico-emacs)
 
 (defun pico-sentinel (process event)
    (princ
@@ -20,45 +40,44 @@
 
 (defun kill-buffer-on-exit (process event)
   (when (string-equal "finished\n" event)
-    (unless keep-buffer-on-exit 
+    (unless pico-keep-buffer-open 
       (kill-buffer (process-buffer process)))))
-(defvar keep-buffer-on-exit t "Don't kill the process buffer on job finish.")
-
-(defvar target-type (list "-f" "target/rp2350.cfg") "openocd target")
-
-(defvar interface-type (list "-f" "interface/cmsis-dap.cfg") "openocd interface")
 
 (defvar openocd-exec (concat (getenv "OPENOCD_DIR") "/" (getenv "OPENOCD_NAME"))
   "openocd to execute.")
 
-(defun start-openocd-server()
+(defvar pico-keep-buffer-open t "Don't kill the process buffer on job finish.")
+
+(defun pico-start-openocd-server()
   "Start the openocd server"
+  (interactive)
   (make-process :name "openocd-server"
-		:command (append (list openocd-exec) interface-type target-type)
+		:command (list openocd-exec "-f" openocd-interface "-f" openocd-target)
 		:buffer "*openocd-server*"
 		:sentinel 'pico-sentinel)) 
 
-(defun stop-openocd-server ()
+(defun pico-stop-openocd-server ()
   "Stop the openocd server"
+  (interactive)
   (delete-process "openocd-server"))
   
 (defun is-alive-openocd-server()
   "Test if the openocd server is running."
   (process-live-p (get-process "openocd-server")))
 
-(defun upload-elf(elf)
+(defun pico-upload-elf(elf)
   "Upload an elf file to the pico"
   (if (file-exists-p elf)
       (make-process :name "openocd-upload"
-		    :command (append (list openocd-exec) interface-type target-type
-				     (list "-c" "adapter speed 5000" "-c" (concat "program " elf " verify reset exit")))
+		    :command (list openocd-exec  "-f" openocd-interface "-f" openocd-target 
+				     "-c" "adapter speed 5000" "-c" (concat "program " elf " verify reset exit"))
 		    :buffer "*openocd-upload*"
 		    :sentinel 'kill-buffer-on-exit)
 
     (format "file %s cannot be found." elf))) 
 
 
-(defun open-pico-uart-terminal()
+(defun pico-open-uart-terminal()
   "open a connection to the pico uart."
   (interactive)
   (vterm pico-uart)
@@ -67,15 +86,6 @@
    #'(lambda (ch)
        (vterm-send-key (char-to-string ch)))
    pico-uart-connection-string))  
-(defcustom pico-uart "*pico-uart*"
-  "The buffer name for the pico uart connection."
-  :type 'string
-  :group 'pico-emacs)
-
-(defcustom pico-uart-connection-string "minicom -b 115200 -o -D /dev/ttyACM0\n"
-  "The command to connect to pico uart."
-  :type 'string
-  :group 'pico-emacs)
 
 
 (defun pico-sentinel (process event)
@@ -84,50 +94,9 @@
 
 (defun kill-buffer-on-exit (process event)
   (when (string-equal "finished\n" event)
-    (unless keep-buffer-on-exit 
+    (unless pico-keep-buffer-open 
       (kill-buffer (process-buffer process)))))
-(defvar keep-buffer-on-exit t "Don't kill the process buffer on job finish.")
-
-(defvar target-type (list "-f" "target/rp2350.cfg") "openocd target")
-
-(defvar interface-type (list "-f" "interface/cmsis-dap.cfg") "openocd interface")
-
-(defvar openocd-exec (concat (getenv "OPENOCD_DIR") "/" (getenv "OPENOCD_NAME"))
-  "openocd to execute.")
-
-(defun start-openocd-server()
-  "Start the openocd server"
-  (make-process :name "openocd-server"
-		:command (append (list openocd-exec) interface-type target-type)
-		:buffer "*openocd-server*"
-		:sentinel 'pico-sentinel)) 
-
-(defun stop-openocd-server ()
-  "Stop the openocd server"
-  (delete-process "openocd-server"))
-  
-(defun is-alive-openocd-server()
-  "Test if the openocd server is running."
-  (process-live-p (get-process "openocd-server")))
-
-(defun upload-elf(elf)
-  "Upload an elf file to the pico"
-  (if (file-exists-p elf)
-      (make-process :name "openocd-upload"
-		    :command (append (list openocd-exec) interface-type target-type
-				     (list "-c" "adapter speed 5000" "-c" (concat "program " elf " verify reset exit")))
-		    :buffer "*openocd-upload*"
-		    :sentinel 'kill-buffer-on-exit)
-
-    (format "file %s cannot be found." elf))) 
 
 
-(defun open-pico-uart-terminal()
-  "open a connection to the pico uart."
-  (interactive)
-  (vterm pico-uart)
-  (find-buffer-visiting pico-uart)
-  (seq-do
-   #'(lambda (ch)
-       (vterm-send-key (char-to-string ch)))
-   pico-uart-connection-string))  
+
+(provide 'pico-emacs)
